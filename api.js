63cb509a1b61de282d7c820cc0cd4d9bc3e321dd
Nika <nikalyalina239@gmail.com>
2025-04-30 09:05:52 +0500

    breeds on the pages

--- api.js
+++ api.js
@@ -25,3 +25,44 @@ async function fetchBreeds() {// функция для получения все
         return [];
     }
 }
+
+function displayBreeds(breeds) {// функция для отображения пород на странице
+    const resultDiv = document.getElementById('result');
+    const paginationDiv = document.getElementById('pagination');
+
+    if (breeds.length === 0) {
+        resultDiv.innerHTML = 'Ничего не найдено.';
+        paginationDiv.innerHTML = '';
+        return;
+    }
+
+    const totalPages = Math.ceil(breeds.length / itemsPerPage);
+    const start = (currentPage - 1) * itemsPerPage;// вычисляем индекс перовой породы на текущей странице
+    const end = start + itemsPerPage;// вычисляем индекс последней породы на текущей странице
+    const breedsToShow = breeds.slice(start, end);
+
+    resultDiv.innerHTML = '<strong>Результаты:</strong><br><br>' +
+        breedsToShow.map(breed => `
+            <div class="breed">
+                <strong>${breed.name}</strong> (${breed.origin})<br>
+                Тип шерсти: ${breed.hairless === 1 ? 'Лысая' : 'Обычная'}<br>
+                Гипоаллергенная: ${breed.hypoallergenic === 1 ? 'Да' : 'Нет'}<br>
+                Темперамент: ${breed.temperament}<br><br>
+                <img src="${breed.image ? breed.image.url : 'https://via.placeholder.com/150'}" alt="${breed.name}" style="width: 150px; height: auto; border-radius: 8px;">
+            </div>
+        `).join('');
+
+    paginationDiv.innerHTML = '';
+    for (let i = 1; i <= totalPages; i++) {
+        const pageBtn = document.createElement('button');
+        pageBtn.innerText = i;//номер старницы
+        if (i === currentPage) {// если кнопка соответствует текущей странице, добавляем класс active-page
+            pageBtn.classList.add('active-page');
+        }
+        pageBtn.onclick = () => {
+            currentPage = i;
+            displayBreeds(filteredBreeds);
+        };
+        paginationDiv.appendChild(pageBtn);
+    }
+}
